431382deb21a13a97dfe7a0030d0149637667b08
Ali Biner <alibiner.github@gmail.com>
2023-02-25 17:56:49 +0300

    Struct Finished

---
+++ DataStructures/Dots.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public struct Dots
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+
+        //This is Default Constructor but Default Constructor doesn't at struct. default constructor convert to constructor when described parameter 
+        //public Dots( )
+        //{
+            
+        //}
+
+        public Dots(int x, int y)
+        {
+            X = x ;
+            Y = y ;
+        }
+
+        public void SetOrigin()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public void Excahnge()
+        {
+            var ms = X;
+            X = Y;
+            Y = ms;
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -9,50 +9,61 @@ namespace DataStructures
     //Struct - Defining
     public class Program
     {
-        //Struct - Using
         public static void Main(string[] args)
         {
-            //Student std = new Student();
-            //std.Number = 10;
-            //std.FirstName = "Ali";
-            //std.LastName = "Biner";
-            //std.Gender = "Erkek";
-
-            //Student std2 = new Student()
-            //{
-            //    Number = 20,
-            //    FirstName = "Zehra",
-            //    LastName = "Kardelen",
-            //    Gender = "Kadın"
-            //};
-
-            //Student std3 = new Student(30,"Mert","Derviş","Erkek");
-
-            //Method using with non-override
-            //Console.WriteLine($"{std.Number} {std.FirstName} {std.LastName} {std.Gender}" );
-            //Console.WriteLine($"{std2.Number} {std2.FirstName} {std2.LastName} {std2.Gender}" );
-            //Console.WriteLine($"{std3.Number} {std3.FirstName} {std3.LastName} {std3.Gender}" );
-
-            //Method using with override
-            //Console.WriteLine(std);
-            //Console.WriteLine(std2);
-            //Console.WriteLine(std3);
-
-            List<Student> stdList = new List<Student>()
-            {
-                new Student(10,"Ali","Biner"),
-                new Student(20,"Ayşe","Demir","Kadın"),
-                new Student(30,"Mert","Derviş","Erkek")
-            };
-
-            foreach (var VARIABLE in stdList)
-            {
-                Console.WriteLine(VARIABLE);
-            }
-
-            stdList.ForEach(o=> Console.WriteLine(o));
-
+            Dots dt = new Dots(3,4);
+            Console.WriteLine(dt);
+            dt.Excahnge();
+            Console.WriteLine(dt);
+            
             Console.ReadKey();
         }
+
+
+        ////Struct - Using
+        //public static void Main(string[] args)
+        //{
+        //    //Student std = new Student();
+        //    //std.Number = 10;
+        //    //std.FirstName = "Ali";
+        //    //std.LastName = "Biner";
+        //    //std.Gender = "Erkek";
+
+        //    //Student std2 = new Student()
+        //    //{
+        //    //    Number = 20,
+        //    //    FirstName = "Zehra",
+        //    //    LastName = "Kardelen",
+        //    //    Gender = "Kadın"
+        //    //};
+
+        //    //Student std3 = new Student(30,"Mert","Derviş","Erkek");
+
+        //    //Method using with non-override
+        //    //Console.WriteLine($"{std.Number} {std.FirstName} {std.LastName} {std.Gender}" );
+        //    //Console.WriteLine($"{std2.Number} {std2.FirstName} {std2.LastName} {std2.Gender}" );
+        //    //Console.WriteLine($"{std3.Number} {std3.FirstName} {std3.LastName} {std3.Gender}" );
+
+        //    //Method using with override
+        //    //Console.WriteLine(std);
+        //    //Console.WriteLine(std2);
+        //    //Console.WriteLine(std3);
+
+        //    List<Student> stdList = new List<Student>()
+        //    {
+        //        new Student(10,"Ali","Biner"),
+        //        new Student(20,"Ayşe","Demir","Kadın"),
+        //        new Student(30,"Mert","Derviş","Erkek")
+        //    };
+
+        //    foreach (var VARIABLE in stdList)
+        //    {
+        //        Console.WriteLine(VARIABLE);
+        //    }
+
+        //    stdList.ForEach(o=> Console.WriteLine(o));
+
+        //    Console.ReadKey();
+        //}
     }
 }
