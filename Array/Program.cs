cd85951463d2a5c755196670ff7bd182a3ba5d12
Ali Biner <alibiner.github@gmail.com>
2023-02-27 23:04:43 +0300

    Array learned use methods

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Array.CreateInstance(typeof(int), 5);
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.SetValue(rnd.Next(1,11), i);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("numbers[{0}] = {1}",i,numbers.GetValue(i));
+
+            }
+        }
+    }
+}
